30e370d24c963fe8b04879f0bef8c0254bccabd3
HectorEsteban10 <hectorestebanmiranda50@gmail.com>
2025-04-22 11:03:17 +0200

    gf

--- Examen2/p04.js
+++ Examen2/p04.js
@@ -17,4 +17,18 @@ function menor4(a,b,c,d)
     return menor2(m1,m2);
 
 }
-console.log(menor4(4,2,-6,20));
+// console.log(menor4(4,2,-6,20));
+
+
+function menor16(a,b,c,d,e,f,g,h,i,j,k,l,o,m,n,o,p)
+{
+    let m1 = menor4(a,b,c,d);
+    let m2 = menor4(e,f,g,h);
+    let m3 = menor4(i,j,k,l);
+    let m4 = menor4(m,n,o,p);
+
+    return menor4(m1,m2,m3,m4)
+}
+
+console.log(menor16(4,2,-6,20,4,2,-6,20,4,2,-666,20,4,2,-6,20));
+ 
